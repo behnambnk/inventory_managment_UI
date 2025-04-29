4e6a6cf11df665e728523abf114003d898cd9c3b
behzad <b.banikarimi@gmail.com>
2025-04-30 01:33:12 +1000

    fix mantime

--- app/main.jsx
+++ app/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { MantineProvider } from '@mantine/core';
 import { AuthProvider } from './context/AuthContext.tsx';
 import App from './app.jsx';
 import './app.css';
@@ -12,11 +11,8 @@ import './app.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-  
         <AuthProvider>
-        <MantineProvider withGlobalStyles withNormalizeCSS>
           <App />
-        </MantineProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
